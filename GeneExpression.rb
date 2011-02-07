d2e14c3f6b0a9b7edb9d5a0b069296832c3a1dde
Jonathan Badger <jhbadger@gmail.com>
2011-02-07 12:41:20 -0800

    updated GeneExpression

--- GeneExpression.rb
+++ GeneExpression.rb
@@ -1,28 +1,30 @@
 # routines to process gene expression data
 
-
-# generate a poisson random number
-def poisson(lambda)
- l = Math.exp(-lambda)
- k = 0
- p = 1
- while p >= l
-   k += 1
-   u = rand
-   p = p *u
- end
- return k -1
-end
-
-# calculate Strekel (2000) R value
-def rvalue(total, counts)
-  r = 0
-  sum = total.keys.reduce(0) {|s, lib| s += counts[lib]}
-  freq = sum/(1.0*total.values.reduce(:+))
-  total.keys.each do |lib|
-    if (!counts[lib].nil? && counts[lib] > 0)
-      r += (counts[lib]*Math.log((counts[lib])/(total[lib]*freq)))
+class GeneExpression
+  # generate a poisson random number
+  def GeneExpression.poisson(lambda)
+    return lambda if (lambda > 700)
+    l = Math.exp(-lambda)
+    k = 0
+    p = 1
+    while p >= l
+      k += 1
+      u = rand
+      p = p *u
     end
+    return k -1
   end
-  return r
+  
+  # calculate Strekel (2000) R value
+  def GeneExpression.rvalue(total, counts)
+    r = 0
+    sum = counts.reduce(:+)
+    freq = sum/(1.0*total.reduce(:+))
+    total.size.times do |i|
+      if (!counts[i].nil? && counts[i] > 0)
+        r += (counts[i]*Math.log((counts[i])/(total[i]*freq)))
+      end
+    end
+    return (100*r).to_i/100.0 # round nicely
+  end
 end
