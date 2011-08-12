88939035e96b1a0a0d1c7c4c032f9849198c8d11
Jonathan Badger <jhbadger@gmail.com>
2011-08-12 13:19:27 -0700

    new version of pblastNCBI using grid arrays

--- oneoffs/addcolsDataset.rb
+++
@@ -1,7 +0,0 @@
-Dir.glob("*/apis/dataset.tsv").each do |dataset|
-  out = File.new(dataset, "w")
-  File.new(dataset + ".orig").each do |line|
-    name, owner, date_added, database_used, comments, group, username, password = line.chomp.split("\t")
-    out.print [name, owner, date_added, database_used, comments, group, username, password].join("\t") + "\n"
-  end
-end
--- oneoffs/unannotatedPeps.rb
+++
@@ -1,7 +0,0 @@
-require 'bio'
-
-Bio::FlatFile.new(Bio::FastaFormat, File.new(ARGV.first)).each do |seq|
-  if (seq.definition.index(seq.entry_id, 1))
-    print seq
-  end
-end
