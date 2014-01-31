ffd3f055b007b32fb0e344bd07cfc1a0ad4844bd
Jonathan Badger <jhbadger@gmail.com>
2014-01-30 20:20:36 -0500

    dir in Grid

--- Grid.rb
+++ Grid.rb
@@ -2,19 +2,20 @@
 class Grid
   attr_accessor :name
   
-  def initialize(command, project = nil, memory = nil, queue = nil)
+  def initialize(command, project = nil, memory = nil, queue = nil, dir = Dir.pwd)
     @command = [command].flatten
     @name = @command.first.gsub("/","_").split(" ").first
     @project = project
     @memory = memory
     @queue = queue
+    @dir = dir
     @count = 0
   end
   
   # return name for next data file
   def next
     @count += 1
-    filename = @name + "_input." + @count.to_s
+    filename = @dir + "/" + @name + "_input." + @count.to_s
     return filename
   end
   
@@ -23,8 +24,8 @@ class Grid
     out = File.new(@name + ".com", "w")
     out.printf("\#!/bin/sh\n")
     out.printf("\#$ -t 1-%d\n", @count)
-    out.printf("cd %s\n",Dir.pwd)
-    if (@command[1])
+    out.printf("cd %s\n", @dir)
+    if @command[1]
       out.printf("%s %s_input.$SGE_TASK_ID | %s\n", @command.first, 
       @name, @command[1])
     else
@@ -40,7 +41,7 @@ class Grid
     if local
       1.upto(@count).each do |i|
         com = Dir.pwd+"/"+@name+".com"
-        cmd = "export SGE_TASK_ID=#{i};bash #{com} > #{com}.o#{i} 2>#{com}.e#{i}"
+        cmd = "export SGE_TASK_ID=#{i};bash #{@name}.com > #{tmp}/#{com}.o#{i} 2>#{tmp}/#{com}.e#{i}"
         STDERR << "Submitting #{cmd}...\n"
         fork do
           `#{cmd}` 
@@ -78,7 +79,7 @@ class Grid
       File.unlink(file)
     end
     out = File.new(@name + ".out", "w")
-    Dir.glob(@name + ".com.o*").each do |file|
+    Dir.glob(@dir + "/" + @name + ".com.o*").each do |file|
       File.new(file).each do |line|
         out.print line
       end
@@ -86,7 +87,7 @@ class Grid
     end
     out.close
     err = File.new(@name + ".err", "w")
-    Dir.glob(@name + ".com.e*").each do |file|
+    Dir.glob(@dir + "/" + @name + ".com.e*").each do |file|
       File.new(file).each do |line|
         err.print line
       end
